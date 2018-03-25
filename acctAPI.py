45a1d83fdb14429ee74d49d90cb24f50c2763644
woonzh <zhenhao1991@gmail.com>
2018-03-25 17:13:30 +0800

    first commit

--- acctAPI.py
+++ acctAPI.py
@@ -51,7 +51,7 @@ class Accounts(Resource):
                     "names":lst
                     }
             
-            return json.dumps(result)
+            return result
             
             cur.close()
         except ps.Error as e:
