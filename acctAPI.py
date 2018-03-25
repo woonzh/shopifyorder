0d1a36f31e99e5b05dc7c01dad96f21cd62b4431
woonzh <zhenhao1991@gmail.com>
2018-03-25 19:07:21 +0800

    s

--- acctAPI.py
+++ acctAPI.py
@@ -4,6 +4,7 @@ Created on Sat Mar 24 17:34:54 2018
 
 @author: woon.zhenhao
 """
+import flask
 from flask import Flask, request
 from flask_cors import CORS
 from flask_restful import Resource, Api
@@ -52,10 +53,9 @@ class Accounts(Resource):
                     }
             
             cur.close()
-            
-            response=Flask.jsonify(result)
-            response.headers.add('Access-Control-Allow-Origin', '*')
-            return result
+            resp = flask.Response(result)
+            resp.headers['Access-Control-Allow-Origin'] = '*'
+            return resp
         except ps.Error as e:
             msg=e.pgerror
             return msg
