4804925a5bf97333fd0f7d13f467ac3ec53d5203
woonzh <zhenhao1991@gmail.com>
2018-04-09 21:08:04 +0800

    t

--- test.py
+++ test.py
@@ -8,19 +8,19 @@ Created on Sun Mar 25 15:22:54 2018
 import requests
 import json
 
-url='https://shopifyorder.herokuapp.com/orders'
-
-param={"name":"k-bella-swim"}
-
+#url='https://shopifyorder.herokuapp.com/orders'
+#
+#param={"name":"k-bella-swim"}
+#
 #response = requests.get(url, params=param)
 #print(response.content)
 
 param={
-       "name":"name",
-       "email":"email",
-       "apikey":"apikey",
-       "password":"password",
-       "sharedsecret":"sharedsecret"
+       "name":"woonzh",
+       "email":"woonzh@hotmail.com",
+       "apikey":"d65bb311b92444b3ab661639c1ceeee9",
+       "password":"f973e5eabe90af1d7c9f3faeed311f8e",
+       "sharedsecret":"c73d622d538b5e1520967127bda26810"
        }
 
 url='https://shopifyorder.herokuapp.com/createAccount'
