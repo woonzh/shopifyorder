34db7f1453f614e99d4539907ffd1b186f018ba1
woonzh <zhenhao1991@gmail.com>
2018-04-25 22:52:10 +0800

    t

--- acctAPI.py
+++ acctAPI.py
@@ -5,7 +5,7 @@ Created on Sat Mar 24 17:34:54 2018
 @author: woon.zhenhao
 """
 import flask
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template
 from flask_cors import CORS
 from flask_restful import Resource, Api
 from urllib import parse
@@ -18,6 +18,10 @@ import keyRetriever as kr
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
+
+@app.route('/')
+def hello(name=None):
+    return render_template('home.html')
 
 class Common():
     def connectToDatabase(url):
